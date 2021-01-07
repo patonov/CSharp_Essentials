03219c3514e4d226dd22e748587cf4d2c7dec1fd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-07 17:42:12 +0200

    Add files via upload

---
+++ Rectangle Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int area = a * b;
+
+            Console.WriteLine(area);
+        }
+    }
+}
