53a9a4eaddba8bdf4eec6ebcc2cd2c5ab4031f02
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-08 17:28:07 +0200

    Add files via upload

---
+++ Yard Greening.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMeters = double.Parse(Console.ReadLine());
+
+
+            double totalCosts = squareMeters * 7.61;
+            double discount = totalCosts * 0.18;
+            double endPrice = totalCosts - discount;
+
+
+            Console.WriteLine("The final price is: {0:F2} lv.", endPrice);
+            Console.WriteLine("The discount is: {0:F2} lv.", discount);
+
+        }
+    }
+}
