a7b702785062d0755ccc759b399f6e5da82f3dc8
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-08 17:27:41 +0200

    Add files via upload

---
+++ Pet Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsNumber = int.Parse(Console.ReadLine());
+            int otherDogsNumber = int.Parse(Console.ReadLine());
+
+            double costsDodsFood = dogsNumber * 2.50;
+            double costsotherDogsFood = otherDogsNumber * 4.00;
+
+            double endSum = costsDodsFood + costsotherDogsFood;
+
+            Console.WriteLine("{0:F2} lv.", endSum);
+
+        }
+    }
+}
