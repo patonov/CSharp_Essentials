ea0af11f990f73fe8b4b161655a44a6229c40362
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-08 17:27:16 +0200

    Add files via upload

---
+++ Projects Creation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfArchitect = Console.ReadLine();
+            int projectsNumber = int.Parse(Console.ReadLine());
+
+            int timeNeeded = projectsNumber * 3;
+
+
+            Console.WriteLine("The architect {0} will need {1} hours to complete {2} project/s.", nameOfArchitect, timeNeeded, projectsNumber);
+        }
+    }
+}
