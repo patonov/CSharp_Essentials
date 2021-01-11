714ccdd9d70b1fda7fac7b9d5da5fadc2e0653c1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-11 12:50:50 +0200

    Add files via upload

---
+++ Character Sequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
