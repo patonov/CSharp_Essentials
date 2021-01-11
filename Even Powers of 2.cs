9ee84fe6e277314d88865201418a56120456fdbf
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-11 12:50:23 +0200

    Add files via upload

---
+++ Even Powers of 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int power = 0; power <= num; power++)
+            {
+                if (power % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(2, power));
+                }
+            }
+        }
+    }
+}
