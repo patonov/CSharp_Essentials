e0d9aff627f223fe93d9c7337bce17f4742c4eb8
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-12 18:01:45 +0200

    Add files via upload

---
+++ Left and Right Sum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < input; i++)
+            {
+                leftSum = leftSum + int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < input; i++)
+            {
+                rightSum = rightSum + int.Parse(Console.ReadLine());
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine("Yes, sum = {0}", leftSum);
+            }
+            else
+            {
+                int diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine("No, diff = {0}", diff);
+            }
+        }
+    }
+}
