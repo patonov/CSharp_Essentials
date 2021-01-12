e53ec6ea41c3c7dc13c7b45fe189f2e05490c48b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-12 18:03:29 +0200

    Add files via upload

---
+++ Print Numbers in Reverse Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[numbers.Length - i - 1] + " ");
+            }
+        }
+    }
+}
