9446b41e17555ff84a518f88186f2647e27c076d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-12 18:02:09 +0200

    Add files via upload

---
+++ Odd Even Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int number = 0;
+            int evenPositionSum = 0;
+            int oddPositionSum = 0;
+
+            for (int i = 0; i < input; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenPositionSum += number;
+                }
+                else
+                {
+                    oddPositionSum += number;
+                }
+            }
+
+            if (evenPositionSum == oddPositionSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", evenPositionSum);
+            }
+            else
+            {
+                int diff = Math.Abs(evenPositionSum - oddPositionSum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", diff);
+            }
+
+        }
+    }
+}
