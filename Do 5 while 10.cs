c75fe04998c4c1ddbffa58123eb7aaaf436ec306
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-24 21:45:30 +0200

    Add files via upload

---
+++ Do 5 while 10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_5_while_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+                if (i > 5)
+                {
+                    break;
+                }
+
+            } while (i < 10);
+        }
+    }
+}
