306232f6f9b4737c43579a81d60d269bce4b08b8
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-03-20 23:12:31 +0200

    Add files via upload

---
+++ SquarePrinter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquarePrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int n = int.Parse(Console.ReadLine());
+
+			if (n > 0 && n <= 30)
+			{
+				for (int i = 0; i < n; i++)
+				{
+					for (int j = 0; j < n; j++)
+					{
+						Console.Write("# ");
+					}
+					Console.WriteLine();
+				}
+			}
+			else
+			{
+				Console.WriteLine("Try again later!");
+			}
+
+		}
+	}
+}
